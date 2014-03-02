5aad29884a08486c2e5cffe1e3d412dda444a668
Lishi Jiang <lizissleepy@gmail.com>
2014-03-01 18:57:58 -0800

    wrong interpretation about Permutation, Q3 as Q8. submit correct Q3.

--- Chapter_1/Q3/Q3_8.java
+++ Chapter_1/Q3/Q3_8.java
@@ -8,11 +8,12 @@
 
 package Q3;
 
-public class Q3 {
+public class Q3_8 {
 	public static void main(String[] args) {
 		Permutation p = new Permutation(args[0], args[1]);
 		System.out.println("These two strings are: "+p.getStr1()+", "+p.getStr2());
 		System.out.println("Are they permutate: "+p.isPermutation());
+		System.out.println("Are they rotation: "+p.isRotation());
 	}
 }
 
@@ -23,7 +24,28 @@ class Permutation {
 		this.str1 = str1;
 		this.str2 = str2;
 	}
-	boolean isPermutation() {
+	boolean isPermutation() { //it can also be implemented by ASCII
+		if (str1.length() != str2.length())
+			return false;
+		boolean[] find = new boolean[str2.length()]; 
+		char[] char_1 = str1.toCharArray();
+		for (char c : char_1) {
+			int i = 0;
+			for (; i < str2.length(); i++)
+				if (c == str2.charAt(i)) {
+					if (find[i])
+						continue;
+					else {
+						find[i] = true;
+						break;
+					}
+				}
+			if (i == str2.length())
+				return false;
+		}
+		return true;
+	}
+	boolean isRotation() {
 		if (str1.length() != str2.length())
 			return false;
 		String double_string = new String(this.str2);
